0aab7f4bc90d044dceac937951f96ad6c4c5182b
shoaibmirza@123 <mirzashoaibbeg786@gmail.com>
2025-12-15 21:49:03 +0530

    updated iam.tf

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -19,3 +19,17 @@ resource "aws_iam_role_policy_attachment" "ecs_execution_policy" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
+resource "aws_iam_role" "ecs_task" {
+  name = "ecsTaskRole-strapi"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
