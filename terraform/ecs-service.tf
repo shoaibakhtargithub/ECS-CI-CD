7ef12e673cf2d7ac5e7263a0576bd80924da69d9
shoaibmirza@123 <mirzashoaibbeg786@gmail.com>
2025-12-18 21:48:40 +0530

    updated terraform ecs-service for CD

--- terraform/ecs-service.tf
+++ terraform/ecs-service.tf
@@ -2,7 +2,7 @@ resource "aws_ecs_service" "this" {
   name            = "strapi-service"
   cluster         = aws_ecs_cluster.this.id
   task_definition = aws_ecs_task_definition.this.arn
-  desired_count   = 0
+  desired_count   = 1
   launch_type     = "FARGATE"
 
   network_configuration {
