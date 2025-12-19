236891fa7194c0c3e4ceafa91ab51809b85d15ee
shoaibmirza@123 <mirzashoaibbeg786@gmail.com>
2025-12-19 23:49:48 +0530

    added FARGATE_SPOT instead of farget

--- terraform/ecs-cluster.tf
+++ terraform/ecs-cluster.tf
@@ -1,5 +1,12 @@
 resource "aws_ecs_cluster" "this" {
-  //giving nam\
   name = "strapi-ecs-shoaib-cluster"
 
+  capacity_providers = [
+    "FARGATE_SPOT"
+  ]
+
+  default_capacity_provider_strategy {
+    capacity_provider = "FARGATE_SPOT"
+    weight            = 1
+  }
 }
--- terraform/ecs-service.tf
+++ terraform/ecs-service.tf
@@ -3,8 +3,11 @@ resource "aws_ecs_service" "this" {
   cluster         = aws_ecs_cluster.this.id
   task_definition = aws_ecs_task_definition.this.arn
   desired_count   = 1
-  launch_type     = "FARGATE"
 
+  capacity_provider_strategy {
+    capacity_provider = "FARGATE_SPOT"
+    weight            = 1
+  }
 
   network_configuration {
     subnets          = data.aws_subnets.default_vpc_subnets.ids
@@ -13,11 +16,11 @@ resource "aws_ecs_service" "this" {
   }
 
   load_balancer {
-  target_group_arn = aws_lb_target_group.strapi_tg.arn
-  container_name   = "strapi"
-  container_port   = 1337
+    target_group_arn = aws_lb_target_group.strapi_tg.arn
+    container_name   = "strapi"
+    container_port   = 1337
+  }
+
+  depends_on = [aws_lb_listener.http]
 }
 
-depends_on = [aws_lb_listener.http]
-
-}
